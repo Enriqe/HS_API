0acef687366ebf5165361d1492c61124cafa6a7b
EnriqueM <enrique.marr@gmail.com>
2015-06-20 12:56:17 -0500

    Finished modeling comments

--- spec/controllers/api/v1/comments_controller_spec.rb
+++ spec/controllers/api/v1/comments_controller_spec.rb
@@ -14,4 +14,46 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
       expect(json_response[:comments]).to have(4).items
     end
   end
+
+  describe "POST #create" do
+
+    context "when the record is valid" do
+      before(:each) do
+        post :create, link_id: link.id,
+                      comment: { content: FFaker::HipsterIpsum.paragraph }
+      end
+
+      it "renders a comment resource created by the server" do
+        expect(json_response[:comment]).to have_key(:id)
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when the record is not valid" do
+      before(:each) do
+        post :create, link_id: link.id,
+              comment: { content: "" }
+      end
+
+      it "renders the errors from the server" do
+        expect(json_response[:comment]).to have_key(:errors)
+      end
+
+      it { should respond_with 422 }
+    end
+
+  end
+
+
+
+  describe "DELETE #destroy" do
+
+    it "returns a 204 http status" do
+      comment = FactoryGirl.create :comment
+      delete :destroy, id: comment.id
+
+      expect(response.response_code).to be 204
+    end
+  end
 end
