3c0889e587a6496ded9518a984c2254ea01fc387
EnriqueM <enrique.marr@gmail.com>
2015-06-24 19:07:05 -0500

    Adds sessions create action specs

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
       resources :comments, only: [:destroy]
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 
